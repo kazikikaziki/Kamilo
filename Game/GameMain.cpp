e12400aa02a75c260d16946aa2eb3e078ed5410d
kazikikaziki <52305677+kazikikaziki@users.noreply.github.com>
2021-04-16 14:58:01 +0900

    空プロジェクトを移動して CMakeList を追加。そのままプロジェクトをビルドできるように

---
+++ Game/GameMain.cpp
@@ -0,0 +1,14 @@
+﻿#include <Kamilo.h>
+
+using namespace Kamilo;
+
+void GameMain() {
+	K__SetCurrentDirU8(K__GetExecDirU8()); // exe の場所をカレントディレクトリにする
+	KEngineDef def;
+	def.resolution_x = 800;
+	def.resolution_y = 600;
+	if (KEngine::create(&def)) {
+		KEngine::run();
+		KEngine::destroy();
+	}
+}
