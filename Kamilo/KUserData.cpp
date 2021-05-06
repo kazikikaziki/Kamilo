eae3c85ddcdc27e1870ae1903392e96b6ed4d2d4
kazikikaziki <52305677+kazikikaziki@users.noreply.github.com>
2021-05-06 23:34:07 +0900

    KUserData

--- Kamilo/KUserData.cpp
+++ Kamilo/KUserData.cpp
@@ -33,7 +33,7 @@ class CUserDataImpl: public KInspectorCallback {
 
 public:
 	KNamedValues m_Values;
-	std::unordered_map<KPath, int> m_Tags;
+	std::unordered_map<std::string, int> m_Tags;
 
 	CUserDataImpl() {
 		KEngine::addInspectorCallback(this);
@@ -62,14 +62,14 @@ public:
 		ImGui::Unindent();
 	}
 
-	void _clear(const KPathList &keys) {
+	void _clear(const std::vector<std::string> &keys) {
 		for (auto it=keys.begin(); it!=keys.end(); ++it) {
-			const KPath &k = *it;
+			const std::string &k = *it;
 			auto kit = m_Tags.find(k);
 			if (kit != m_Tags.end()) {
 				m_Tags.erase(kit);
 			}
-			m_Values.remove(k.u8());
+			m_Values.remove(k.c_str());
 		}
 	}
 	void clearValues() {
@@ -77,7 +77,7 @@ public:
 		m_Tags.clear();
 	}
 	void clearValuesByTag(int tag) {
-		KPathList keys;
+		std::vector<std::string> keys;
 		for (auto it=m_Tags.begin(); it!=m_Tags.end(); ++it) {
 			if (it->second == tag) {
 				keys.push_back(it->first);
@@ -86,7 +86,7 @@ public:
 		_clear(keys);
 	}
 	void clearValuesByPrefix(const char *prefix) {
-		KPathList keys;
+		std::vector<std::string> keys;
 		for (int i=0; i<m_Values.size(); i++) {
 			const char *key = m_Values.getName(i);
 			if (KStringUtils::startsWith(key, prefix)) {
@@ -95,32 +95,32 @@ public:
 		}
 		_clear(keys);
 	}
-	int getKeys(KPathList *keys) const {
-		if (keys) {
+	int getKeys(std::vector<std::string> *p_keys) const {
+		if (p_keys) {
 			for (int i=0; i<m_Values.size(); i++) {
 				const char *key = m_Values.getName(i);
-				keys->push_back(key);
+				p_keys->push_back(key);
 			}
 		}
 		return m_Values.size();
 	}
-	bool hasKey(const KPath &key) const {
-		return m_Values.getString(key.u8()) != nullptr;
+	bool hasKey(const std::string &key) const {
+		return m_Values.getString(key.c_str()) != nullptr;
 	}
-	KPath getString(const KPath &key, const KPath &def) const {
-		return m_Values.getString(key.u8(), def.u8());
+	std::string getString(const std::string &key, const std::string &def) const {
+		return m_Values.getString(key.c_str(), def.c_str());
 	}
-	void setString(const KPath &key, const KPath &val, int tag) {
-		m_Values.setString(key.u8(), val.u8());
+	void setString(const std::string &key, const std::string &val, int tag) {
+		m_Values.setString(key.c_str(), val.c_str());
 		m_Tags[key] = tag;
 	}
-	bool saveToFileEx(const KNamedValues *nv, const KPath &filename, const char *password) {
+	bool saveToFileEx(const KNamedValues *nv, const std::string &filename, const std::string &password) {
 		bool ret = false;
 	
-		KOutputStream output = KOutputStream::fromFileName(filename.u8());
+		KOutputStream output = KOutputStream::fromFileName(filename);
 		if (output.isOpen()) {
 			std::string u8 = nv->saveToString();
-			if (password && password[0]) {
+			if (!password.empty()) {
 				u8 = encryptString(u8, password);
 			}
 			if (!u8.empty()) {
@@ -130,13 +130,13 @@ public:
 		}
 		return ret;
 	}
-	bool saveToFile(const KPath &filename, const char *password) {
+	bool saveToFile(const std::string &filename, const std::string &password) {
 		return saveToFileEx(&m_Values, filename, password);
 	}
-	bool saveToFileCompress(const KPath &filename) {
+	bool saveToFileCompress(const std::string &filename) {
 		return saveToFileCompressEx(&m_Values, filename);
 	}
-	bool saveToFileCompressEx(const KNamedValues *nv, const KPath &filename) {
+	bool saveToFileCompressEx(const KNamedValues *nv, const std::string &filename) {
 		if (nv == nullptr) return false;
 		std::string u8 = nv->saveToString();
 		if (u8.empty()) return false;
@@ -144,7 +144,7 @@ public:
 		std::string zbin = KZlib::compress_raw(u8, 1);
 		if (zbin.empty()) return false;
 
-		KOutputStream output = KOutputStream::fromFileName(filename.u8());
+		KOutputStream output = KOutputStream::fromFileName(filename);
 		if (output.isOpen()) {
 			output.writeUint16((uint16_t)u8.size());
 			output.writeUint16((uint16_t)zbin.size());
@@ -153,14 +153,14 @@ public:
 		}
 		return false;
 	}
-	bool loadFromFile(const KPath &filename, const char *password) {
+	bool loadFromFile(const std::string &filename, const std::string &password) {
 		return peekFile(filename, password, &m_Values);
 	}
-	bool loadFromFileCompress(const KPath &filename) {
+	bool loadFromFileCompress(const std::string &filename) {
 		return loadFromFileCompressEx(&m_Values, filename);
 	}
-	bool loadFromFileCompressEx(KNamedValues *nv, const KPath &filename) const {
-		KInputStream file = KInputStream::fromFileName(filename.u8());
+	bool loadFromFileCompressEx(KNamedValues *nv, const std::string &filename) const {
+		KInputStream file = KInputStream::fromFileName(filename);
 		if (!file.isOpen()) return false;
 
 		uint16_t uzsize = file.readUint16();
@@ -169,7 +169,7 @@ public:
 		std::string text_u8 = KZlib::uncompress_raw(zbin, uzsize);
 		text_u8.push_back(0); // 終端文字を追加
 		if (nv) {
-			return nv->loadFromString(text_u8.c_str(), filename.u8());
+			return nv->loadFromString(text_u8.c_str(), filename.c_str());
 		} else {
 			return true;
 		}
@@ -182,16 +182,16 @@ public:
 		}
 		return false;
 	}
-	bool peekFile(const KPath &filename, const char *password, KNamedValues *nv) const {
+	bool peekFile(const std::string &filename, const std::string &password, KNamedValues *nv) const {
 		bool ret = false;
-		KInputStream file = KInputStream::fromFileName(filename.u8());
+		KInputStream file = KInputStream::fromFileName(filename);
 		if (file.isOpen()) {
 			std::string u8 = file.readBin();
-			if (password && password[0]) {
+			if (!password.empty()) {
 				u8 = decryptString(u8, password);
 			}
 			if (nv) {
-				nv->loadFromString(u8.c_str(), filename.u8());
+				nv->loadFromString(u8.c_str(), filename.c_str());
 			}
 			ret = true;
 		}
@@ -225,47 +225,49 @@ void KUserData::clearValuesByPrefix(const char *prefix) {
 	K__Assert(g_UserData);
 	g_UserData->clearValuesByPrefix(prefix);
 }
-KPath KUserData::getString(const KPath &key, const KPath &def) {
+std::string KUserData::getString(const std::string &key, const std::string &def) {
 	K__Assert(g_UserData);
 	return g_UserData->getString(key, def);
 }
-void KUserData::setString(const KPath &key, const KPath &val, int tag) {
+void KUserData::setString(const std::string &key, const std::string &val, int tag) {
 	K__Assert(g_UserData);
 	g_UserData->setString(key, val, tag);
 }
-bool KUserData::hasKey(const KPath &key) {
+bool KUserData::hasKey(const std::string &key) {
 	K__Assert(g_UserData);
 	return g_UserData->hasKey(key);
 }
-int KUserData::getKeys(KPathList *keys) {
+int KUserData::getKeys(std::vector<std::string> *p_keys) {
 	K__Assert(g_UserData);
-	return g_UserData->getKeys(keys);
+	return g_UserData->getKeys(p_keys);
 }
-int KUserData::getInt(const KPath &key, int def) {
-	KPath s = getString(key);
-	return KStringUtils::toInt(s.u8(), def);
+int KUserData::getInt(const std::string &key, int def) {
+	std::string s = getString(key);
+	int ret = def;
+	K::strToInt(s, &ret);
+	return ret;
 }
-void KUserData::setInt(const KPath &key, int val, int tag) {
-	KPath pval = KPath::fromFormat("%d", val);
+void KUserData::setInt(const std::string &key, int val, int tag) {
+	std::string pval = std::to_string(val);
 	setString(key, pval, tag);
 }
-bool KUserData::saveToFile(const KPath &filename, const char *password) {
+bool KUserData::saveToFile(const std::string &filename, const char *password) {
 	K__Assert(g_UserData);
 	return g_UserData->saveToFile(filename, password);
 }
-bool KUserData::saveToFileCompress(const KPath &filename) {
+bool KUserData::saveToFileCompress(const std::string &filename) {
 	K__Assert(g_UserData);
 	return g_UserData->saveToFileCompress(filename);
 }
-bool KUserData::loadFromFile(const KPath &filename, const char *password) {
+bool KUserData::loadFromFile(const std::string &filename, const char *password) {
 	K__Assert(g_UserData);
 	return g_UserData->loadFromFile(filename, password);
 }
-bool KUserData::loadFromFileCompress(const KPath &filename) {
+bool KUserData::loadFromFileCompress(const std::string &filename) {
 	K__Assert(g_UserData);
 	return g_UserData->loadFromFileCompress(filename);
 }
-bool KUserData::peekFile(const KPath &filename, const char *password, KNamedValues *nv) {
+bool KUserData::peekFile(const std::string &filename, const char *password, KNamedValues *nv) {
 	K__Assert(g_UserData);
 	return g_UserData->peekFile(filename, password, nv);
 }
--- Kamilo/KUserData.h
+++ Kamilo/KUserData.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
-#include "KString.h"
+#include <string>
+#include <vector>
 
 namespace Kamilo {
 
@@ -12,23 +13,23 @@ public:
 	static void clearValues(); /// すべての値を削除する
 	static void clearValuesByTag(int tag); /// 指定されたタグの値をすべて削除する
 	static void clearValuesByPrefix(const char *prefix); /// 指定された文字で始まる名前の値をすべて削除する
-	static KPath getString(const KPath &key, const KPath &def=""); /// 文字列を得る
-	static void setString(const KPath &key, const KPath &val, int tag=0); /// 文字列を設定する
-	static bool hasKey(const KPath &key); /// キーが存在するかどうか
-	static int getKeys(KPathList *keys); /// 全てのキーを得る
-	static int getInt(const KPath &key, int def=0);
-	static void setInt(const KPath &key, int val, int tag=0);
+	static std::string getString(const std::string &key, const std::string &def=""); /// 文字列を得る
+	static void setString(const std::string &key, const std::string &val, int tag=0); /// 文字列を設定する
+	static bool hasKey(const std::string &key); /// キーが存在するかどうか
+	static int getKeys(std::vector<std::string> *p_keys); /// 全てのキーを得る
+	static int getInt(const std::string &key, int def=0);
+	static void setInt(const std::string &key, int val, int tag=0);
 
 	/// 値を保存する。password に文字列を指定した場合はファイルを暗号化する
-	static bool saveToFile(const KPath &filename, const char *password="");
-	static bool saveToFileCompress(const KPath &filename);
+	static bool saveToFile(const std::string &filename, const char *password="");
+	static bool saveToFileCompress(const std::string &filename);
 
 	/// 値をロードする。暗号化されている場合は password を指定する必要がある
-	static bool loadFromFile(const KPath &filename, const char *password="");
-	static bool loadFromFileCompress(const KPath &filename);
+	static bool loadFromFile(const std::string &filename, const char *password="");
+	static bool loadFromFileCompress(const std::string &filename);
 
 	/// 実際にロードせずに、保存内容だけを見る
-	static bool peekFile(const KPath &filename, const char *password, KNamedValues *nv);
+	static bool peekFile(const std::string &filename, const char *password, KNamedValues *nv);
 };
 
 
