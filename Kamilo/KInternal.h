db9d1c5582cdde8db4491a46b60ea697b9217861
kazikikaziki <52305677+kazikikaziki@users.noreply.github.com>
2021-05-06 13:56:05 +0900

    KInternal の基本関数を class K に集めた

--- Kamilo/KInternal.h
+++ Kamilo/KInternal.h
@@ -38,8 +38,6 @@ bool K__IsDebuggerPresent();
 void K__Dialog(const char *u8);
 void K__Notify(const char *u8);
 
-
-
 void K__RawPrintf(const char *fmt_u8, ...);
 void K__DebugPrint(const char *fmt_u8, ...);
 void K__Print(const char *fmt_u8, ...);
@@ -57,18 +55,79 @@ void K__SetPrintHook(void (*hook)(const char *u8));
 void K__SetWarningHook(void (*hook)(const char *u8));
 void K__SetErrorHook(void (*hook)(const char *u8));
 
+// HRESULT hr の値からメッセージを得る
+bool K__Win32GetErrorString(long hr, char *buf, int size);
+std::string K__Win32GetErrorStringStd(long hr);
+
+
+
+
+
+//----------------------------------------------------
+// clock
+//----------------------------------------------------
 uint64_t K__ClockNano64(); ///< システム時刻をナノ秒単位で得る
 inline uint64_t K__ClockMsec64() { return K__ClockNano64() / 1000000; } ///< システム時刻をミリ秒単位で取得する
 inline uint32_t K__ClockMsec32() { return (uint32_t)K__ClockMsec64(); } ///< システム時刻をミリ秒単位で取得する
 
 
+//----------------------------------------------------
+// sleep
+//----------------------------------------------------
 void K__SleepPeriodBegin(); ///< タイマーの精度を変更する
 void K__SleepPeriodEnd();
 void K__Sleep(int msec);
 
+
+//----------------------------------------------------
+// numeric
+//----------------------------------------------------
+float K__DegToRad(float deg);
+float K__RadToDeg(float rad);
+float K__Min(float a, float b);
+float K__Max(float a, float b);
+float K__Lerp(float a, float b, float t);
+int K__Min(int a, int b);
+int K__Max(int a, int b);
+
+
+//----------------------------------------------------
+// file
+//----------------------------------------------------
+FILE * K__fopen_u8(const char *path_u8, const char *mode_u8);
+bool K__ShellOpenU8(const char *path_u8);
+std::string K__LoadStringFromFile(const char *filename_u8);
+void K__SaveStringToFile(const char *filename_u8, const std::string &bin);
+
+
+//----------------------------------------------------
+// sys
+//----------------------------------------------------
+uint32_t K__pid(); ///< プロセスIDを得る
+uint32_t K__GetCurrentThreadId();
+void K__getcwd_u8(char *out_u8, int maxsize); ///< カレントディレクトリを UTF8 で得る
+bool K__setcwd_u8(const char *path_u8);
+std::string K__GetExecDirU8();
+std::string K__GetCurrentDirU8();
+void K__SetCurrentDirU8(const std::string &dir);
+void K__selfpath_u8(char *out_u8, int maxsize); ///< 自分の名前を得る（実行ファイル名）
+
+
+//----------------------------------------------------
+// path
+//----------------------------------------------------
+std::string K__PathJoin(const std::string &s1, const std::string &s2);
+std::string K__PathRenameExtension(const std::string &path, const std::string &ext);
+int K__PathCompare(const char *path1, const char *path2, bool ignore_case, bool ignore_path);
+void K__fullpath_u8(char *out_u8, int maxsize, const char *path_u8);
+
+
+
+//----------------------------------------------------
+// string
+//----------------------------------------------------
 const char * K__SkipUtf8Bom(const char *s);
 bool K__StartsWithUtf8Bom(const void *data, int size);
-
 int K__StrFind(const char *s, const char *sub, int start);
 void K__Replace(std::string &s, int start, int count, const char *str);
 void K__Replace(std::string &s, const char *before, const char *after);
@@ -81,23 +140,9 @@ bool K__strtoi(const char *s, int *val);
 bool K__strtoui32(const char *s, uint32_t *val);
 bool K__strtoui64(const char *s, uint64_t *val);
 int K__stricmp(const char *s, const char *t); ///< _stricmp または strcasecmp のエイリアス
-uint32_t K__pid(); ///< プロセスIDを得る
-void K__getcwd_u8(char *out_u8, int maxsize); ///< カレントディレクトリを UTF8 で得る
-bool K__setcwd_u8(const char *path_u8);
-FILE * K__fopen_u8(const char *path_u8, const char *mode_u8);
-void K__selfpath_u8(char *out_u8, int maxsize); ///< 自分の名前を得る（実行ファイル名）
-void K__fullpath_u8(char *out_u8, int maxsize, const char *path_u8);
-std::string K__GetExecDirU8();
-std::string K__GetCurrentDirU8();
-void K__SetCurrentDirU8(const std::string &dir);
-bool K__ShellOpenU8(const char *path_u8); ///< 指定されたファイルまたはフォルダをシステム標準の方法で開く
 char * K__strptime_l(const char *str, const char *fmt, struct tm *out_tm, const char *_locale);
-
 std::string K__vsprintf_std(const char *fmt, va_list args);
 std::string K__sprintf_std(const char *fmt, ...);
-std::string K__PathJoin(const std::string &s1, const std::string &s2);
-std::string K__PathRenameExtension(const std::string &path, const std::string &ext);
-int K__PathCompare(const char *path1, const char *path2, bool ignore_case, bool ignore_path);
 
 bool K__iswprint(wchar_t wc); // 印字文字（空白を含む）
 bool K__iswgraph(wchar_t wc); // 印字文字（空白を含まない）
@@ -125,27 +170,6 @@ std::wstring K__AnsiToWideStd(const std::string &ansi, const char *_locale);
 
 void K__Utf8ToWidePath(wchar_t *out_wpath, int num_wchars, const char *path_u8);
 void K__WideToUtf8Path(char *out_path_u8, int num_bytes, const wchar_t *wpath);
-
-
-// HRESULT hr の値からメッセージを得る
-bool K__Win32GetErrorString(long hr, char *buf, int size);
-std::string K__Win32GetErrorStringStd(long hr);
-
-
-std::string K__LoadStringFromFile(const char *filename_u8);
-void K__SaveStringToFile(const char *filename_u8, const std::string &bin);
-
-uint32_t K__GetCurrentThreadId();
-
-float K__DegToRad(float deg);
-float K__RadToDeg(float rad);
-float K__Min(float a, float b);
-float K__Max(float a, float b);
-float K__Lerp(float a, float b, float t);
-int K__Min(int a, int b);
-int K__Max(int a, int b);
-
-
 
 
 struct _StrW {
@@ -180,6 +204,118 @@ template <class... Args> void K__OutputDebugString(Args... args) {
 	_OutputW(ws);
 	_OutputW(L"\n");
 }
+
+class K {
+public:
+	#pragma region clock
+	static uint64_t clockNano64() { return K__ClockNano64(); } ///< システム時刻をナノ秒単位で得る
+	static uint64_t clockMsec64() { return K__ClockNano64() / 1000000; } ///< システム時刻をミリ秒単位で取得する
+	static uint32_t clockMsec32() { return (uint32_t)K__ClockMsec64(); } ///< システム時刻をミリ秒単位で取得する
+	#pragma endregion // clock
+
+	#pragma region sleep
+	void sleepPeriodBegin() { K__SleepPeriodBegin(); } ///< タイマーの精度を変更する
+	void sleepPeriodEnd() { K__SleepPeriodEnd(); }
+	void sleep(int msec) { K__Sleep(msec); }
+	#pragma endregion // sleep
+
+	#pragma region numeric
+	static float degToRad(float deg) { return K__DegToRad(deg); }
+	static float radToDeg(float rad) { return K__RadToDeg(rad); }
+	static float lerp(float a, float b, float t) { return K__Lerp(a, b, t); }
+	static float min(float a, float b) { return K__Min(a, b); }
+	static float max(float a, float b) { return K__Max(a, b); }
+	static int min(int a, int b) { return K__Min(a, b); } // ※ここでエラーが起きる場合はNOMINMAXの定義を忘れていて windows.h の min, max と競合している可能性がある
+	static int max(int a, int b) { return K__Max(a, b); }
+	#pragma endregion // numeric
+
+	#pragma region file
+	static bool fileShellOpen(const std::string &path_u8) { return K__ShellOpenU8(path_u8.c_str()); }
+	static FILE * fileOpen(const std::string &path_u8, const std::string &mode_u8) { K__fopen_u8(path_u8.c_str(), mode_u8.c_str()); }
+	static std::string fileLoadString(const std::string &filename) { return K__LoadStringFromFile(filename.c_str()); }
+	static void fileSaveString(const std::string &filename, const std::string &bin) { K__SaveStringToFile(filename.c_str(), bin); }
+	#pragma endregion // file
+
+	#pragma region sys
+	static uint32_t sysGetCurrentProcessId() { return K__pid(); } ///< 現在のプロセスIDを得る
+	static uint32_t sysGetCurrentThraedId() { return K__GetCurrentThreadId(); } ///< 現在のスレッドIDを得る
+	static std::string sysGetCurrentDir() { return K__GetCurrentDirU8(); } ///< カレントディレクトリを UTF8 で得る
+	static void sysSetCurrentDir(const std::string &dir) { K__SetCurrentDirU8(dir); }
+	static std::string sysGetCurrentExecName() { ///< 自分自身（実行ファイル）のファイル名を得る
+		char s[256] = {0};
+		K__selfpath_u8(s, sizeof(s));
+		return s;
+	}
+	static std::string sysGetCurrentExecDir() { ///< 自分自身（実行ファイル）の親ディレクトリを得る
+		return K__GetExecDirU8();
+	}
+	#pragma endregion // sys
+
+	#pragma region path
+	static std::string pathJoin(const std::string &s1, const std::string &s2) { return K__PathJoin(s1, s2); }
+	static std::string pathRenameExtension(const std::string &path, const std::string &ext) { return K__PathRenameExtension(path, ext); }
+	static int pathCompare(const char *path1, const char *path2, bool ignore_case, bool ignore_path) { K__PathCompare(path1, path2, ignore_case, ignore_path); }
+	static std::string pathGetFull(const std::string &s) {
+		char t[256] = {0};
+		K__fullpath_u8(t, sizeof(t), s.c_str());
+		return t;
+	}
+	#pragma endregion // path
+
+	#pragma region string
+	static const char * strSkipBom(const char *s) { return K__SkipUtf8Bom(s); }
+	static bool strStartsWithBom(const void *data, int size) { return K__StartsWithUtf8Bom(data, size); }
+	static int strFind(const char *s, const char *sub, int start) { return K__StrFind(s, sub, start); }
+	static void strReplace(std::string &s, int start, int count, const char *str) { K__Replace(s, start, count, str); }
+	static void strReplace(std::string &s, const char *before, const char *after) { K__Replace(s, before, after); }
+	static void strReplaceW(wchar_t *s, wchar_t before, wchar_t after) { K__ReplaceW(s, before, after); }
+	static void strReplaceA(char *s, char before, char after) { K__ReplaceA(s, before, after); }
+	static bool strToFloat(const std::string &s, float *val) { return K__strtof(s, val); }
+	static bool strToFloat(const char *s, float *val) { return K__strtof(s, val); }
+	static bool strToInt(const std::string &s, int *val) { return K__strtoi(s, val); }
+	static bool strToInt(const char *s, int *val) { return K__strtoi(s, val); }
+	static bool strToUInt32(const char *s, uint32_t *val) { return K__strtoui32(s, val); }
+	static bool strToUInt64(const char *s, uint64_t *val) { return K__strtoui64(s, val); }
+	static int strCaseCmp(const char *s, const char *t) { return K__stricmp(s, t); }
+	static char * strParseTime(const char *str, const char *fmt, struct tm *out_tm, const char *_locale) { return K__strptime_l(str, fmt, out_tm, _locale); }
+	static std::string vsprintf_std(const char *fmt, va_list args) { return K__vsprintf_std(fmt, args); }
+	static std::string sprintf_std(const char *fmt, ...) {
+		va_list args;
+		va_start(args, fmt);
+		std::string result = K__vsprintf_std(fmt, args);
+		va_end(args);
+		return result;
+	}
+
+	static bool isPrintW(wchar_t wc) { return K__iswprint(wc); }
+	static bool isGraphW(wchar_t wc) { return K__iswgraph(wc); }
+	static bool isBlankW(wchar_t wc) { return K__iswblank(wc); }
+	static bool isHalfW(wchar_t wc) { return K__iswhalf(wc); }
+
+	// utf8 ==> wide
+	static int strUtf8ToWide(wchar_t *out_ws, int max_out_widechars, const char *u8, int u8bytes) { return K__Utf8ToWide(out_ws, max_out_widechars, u8, u8bytes); }
+	static std::wstring strUtf8ToWideStd(const std::string &u8) { return K__Utf8ToWideStd(u8); }
+
+	// wide ==> utf8
+	static int strWideToUtf8(char *out_u8, int max_out_bytes, const wchar_t *ws) { return K__WideToUtf8(out_u8, max_out_bytes, ws); }
+	static std::string strWideToUtf8Std(const std::wstring &ws) { return K__WideToUtf8Std(ws); }
+
+	// wide ==> ansi
+	static int strWideToAnsi(char *out_ansi, int max_out_bytes, const wchar_t *ws, const char *_locale) { return K__WideToAnsi(out_ansi, max_out_bytes, ws, _locale); }
+	static int strWideToAnsiL(char *out_ansi, int max_out_bytes, const wchar_t *ws, _locale_t loc) { return K__WideToAnsiL(out_ansi, max_out_bytes, ws, loc); }
+	static std::string strWideToAnsiStd(const std::wstring &ws, const char *_locale) { return K__WideToAnsiStd(ws, _locale); }
+
+	// ansi ==> wide
+	static int strAnsiToWide(wchar_t *out_wide, int max_out_wchars, const char *ansi, const char *_locale) { return K__AnsiToWide(out_wide, max_out_wchars, ansi, _locale); }
+	static int strAnsiToWideL(wchar_t *out_wide, int max_out_wchars, const char *ansi, _locale_t loc) { return K__AnsiToWideL(out_wide, max_out_wchars, ansi, loc); }
+	static std::wstring strAnsiToWideStd(const std::string &ansi, const char *_locale) { return K__AnsiToWideStd(ansi, _locale); }
+
+	static void strUtf8ToWidePath(wchar_t *out_wpath, int num_wchars, const char *path_u8) { K__Utf8ToWidePath(out_wpath, num_wchars, path_u8); }
+	static void strWideToUtf8Path(char *out_path_u8, int num_bytes, const wchar_t *wpath) { K__WideToUtf8Path(out_path_u8, num_bytes, wpath); }
+	#pragma endregion // string
+};
+
+
 
 
 } // namespace
