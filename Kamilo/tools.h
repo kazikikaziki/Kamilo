060edc21ae6be2b8f0c923f29e79ae6719ea8bb9
kazikikaziki <52305677+kazikikaziki@users.noreply.github.com>
2021-07-06 19:09:06 +0900

    tools.h/cpp

---
+++ Kamilo/tools.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <string>
+
+/// Pac ファイル情報を書き出す
+bool ez_ExportPacFileInfo(const std::string &pac_filename_u8);
+
+/// XLSX 内のテキストを抜き出す
+bool ez_ExportTextFromXLSX(const std::string &xlsx_filename);
+
+/// EDGE2 で出力した PAL ファイルを XML でエクスポートする
+bool ez_ExportPalXmlFromEdge2(const std::string &edg_filename);
+
+/// EDGE2 ファイルの中身をエクスポートする
+bool ez_ExportEdge2(const std::string &edg_filename);
+
+/// フォルダ内の .png を全てパックする
+bool ez_PackPngInDir(const std::string &dir);
