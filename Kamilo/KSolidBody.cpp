4a47d188698e8d6faee0aeee98cdbb64e5bc01b1
kazikikaziki <52305677+kazikikaziki@users.noreply.github.com>
2021-09-07 17:49:20 +0900

    position calc typo

--- Kamilo/KSolidBody.cpp
+++ Kamilo/KSolidBody.cpp
@@ -1519,7 +1519,7 @@ private:
 		dyNode->m_Desc._prev_pos = pos;
 
 		// 座標変更
-		KVec3 newpos = pos += spd;
+		KVec3 newpos = pos + spd;
 		KVec3 newspd = spd;
 		if (dyNode && dyNode->getShape()) {
 			newspd.y -= dyNode->m_Desc.get_gravity();
