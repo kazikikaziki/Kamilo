7322aaa3e0648fc2e1451159e712a1d8102b36d8
kazikikaziki <doveilbagno@gmail.com>
2022-01-06 22:26:20 +0900

    ビルドスクリプト visual studio 2022 に対応

--- make_visual_studio_project_files.py
+++ make_visual_studio_project_files.py
@@ -13,43 +13,95 @@
 import os
 import subprocess
 import shutil
+import traceback
 
 # cmake 用の作業フォルダ名
 output_dir = "__cmake"
 
 
-def call(cmd):
-	subprocess.check_call(cmd, shell=True)
+# 最初のカレントフォルダ
+home_dir = os.getcwd()
 
 
-# cmake 用の作業フォルダをいったん削除しておく（ビルドテストの時にキャッシュを使わせないため）
-if os.path.isdir(output_dir):
-	# 安全のため、絶対パスでの指定は拒否する
-	if os.path.isabs(output_dir):
-		raise RuntimeError
+
+
+
+#----------------------------------------------------
+# コマンドラインを実行する
+def execute(cmd):
+	print(">>>> ")
+	print(">>>> " + cmd)
+	print(">>>> ")
+	try:
+		subprocess.check_call(cmd, shell=True)
+		return True
+	except:
+		return False
+
+
+#----------------------------------------------------
+# cmake 用の作業フォルダを作成する
+def make_cmake_working_dir():
+	if not os.path.isdir(output_dir):
+		os.mkdir(output_dir)
+
+
+#----------------------------------------------------
+# cmake 用の作業フォルダを削除する
+def remove_cmake_working_dir():
+	if os.path.isdir(output_dir):
+		# 安全のため、絶対パスでの指定は拒否する
+		if os.path.isabs(output_dir):
+			raise RuntimeError
+		
+		# 安全のため、上方向へのパスを含む相対パスを拒否する
+		if ".." in output_dir: # ..を含むフォルダ名も弾くことになるが、良い名前とは言えないので、それで良しとする
+			raise RuntimeError
+		
+		shutil.rmtree(output_dir)
+
+
+#----------------------------------------------------
+# cmake ファイルを作成。
+def execute_cmake(args):
+	# 初期フォルダに移動
+	os.chdir(home_dir)
+
+	# 既存の cmake 作業フォルダを削除
+	remove_cmake_working_dir();
+
+	# cmake 作業フォルダを作成
+	make_cmake_working_dir();
 	
-	# 安全のため、上方向へのパスを含む相対パスを拒否する
-	if ".." in output_dir: # ..を含むフォルダ名も弾くことになるが、良い名前とは言えないので、それで良しとする
-		raise RuntimeError
+	# cmake 作業フォルダ内に移動
+	os.chdir(output_dir) # 作業フォルダ内に移動
+
+	# cmake を実行
+	dir = ".." # CMakeLists.txt のあるフォルダ（現在のひとつ上のフォルダ）を指定する
+	if execute("cmake " + dir + " " + args):
+		# 成功
+		return True
 	
-	shutil.rmtree(output_dir)
+	else:
+		# 失敗した。cmake 作業フォルダを消す
+		remove_cmake_working_dir();
+		return False
 
 
-# cmake 用の作業フォルダを作成
-if not os.path.isdir(output_dir):
-	os.mkdir(output_dir)
+#----------------------------------------------------
+def execute_cmake_try():
+	if execute_cmake("-G\"Visual Studio 16 2019\" -AWin32"): # 32ビット版を作成する
+		return True
+	if execute_cmake("-G\"Visual Studio 17 2022\" -AWin32"):
+		return True
+	return False
 
 
-# 作業フォルダ内に移動
-os.chdir(output_dir)
-
-
+#----------------------------------------------------
 try:
-	# cmake ファイルを作成。CMakeLists.txt のあるフォルダ（現在のひとつ上のフォルダ）を指定する
-	call("cmake .. -G\"Visual Studio 16 2019\" -AWin32") # 32ビット版を作成する
-
-	# ついでに exe のビルドテストもする
-	call("cmake --build .")
+	if execute_cmake_try():
+		execute("cmake --build .") # ついでに exe のビルドテストもする
+		
 except:
 	traceback.print_exc()
 
